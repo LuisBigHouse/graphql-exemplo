aae9b38dd21fead48942430ef5386607c6836289
LuisBigHouse <70987212+LuisBigHouse@users.noreply.github.com>
2021-09-27 13:48:03 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/graphql_exemplo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.graphql_exemplo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final HttpLink rickAndMortyHttpLink =
+        HttpLink('https://rickandmortyapi.com/graphql');
+    ValueNotifier<GraphQLClient> client = ValueNotifier(
+      GraphQLClient(
+        link: rickAndMortyHttpLink,
+        cache: GraphQLCache(
+          store: InMemoryStore(),
+        ),
+      ),
+    );
+
+    String carachterName = "Poopybutthole";
+
+    final rickCharacters = '''
+ query characters {
+      characters(page: 1, filter: { name: "$carachterName" }) {
+        info {
+          count
+        }
+        results {
+          name
+          status
+          location{
+            name
+          }
+        }
+      }
+  }
+  ''';
+
+    return GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+        title: 'Material App',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Complete GraphQl Guide'),
+            backgroundColor: Colors.black,
+          ),
+          body: Query(
+            options: QueryOptions(
+              document: gql(rickCharacters),
+            ),
+            builder: (result, {fetchMore, refetch}) {
+              if (result.isLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (result.data == null) {
+                return Center(
+                  child: Text('Sorry! no character whith name $carachterName'),
+                );
+              } else {
+                return Column(
+                  children: [
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Characters with name $carachterName',
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: result.data!['characters']['results'].length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(
+                                "Name: ${result.data!['characters']['results'][index]['name']}"),
+                            subtitle: Text(
+                                "Status: ${result.data!['characters']['results'][index]['status']} \nLocation:${result.data!['characters']['results'][index]['location']['name']}"),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
